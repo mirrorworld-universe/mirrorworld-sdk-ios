1f1a5d9c4178870454102f02a0c32c750db11062
zmg <zhangmingguang@datenight.cn>
2022-11-21 11:50:22 +0800

    update API Methods

--- Marketplace/MirrorMarketplaceMoudle.swift
+++ Marketplace/MirrorMarketplaceMoudle.swift
@@ -7,30 +7,6 @@
 
 import UIKit
 
-
-
-public typealias onSuccess = ((_ data:String?)->Void)?
-public typealias onFailed = ((_ code:Int,_ message:String?)->Void)?
-@objc public class MirrorBaseMoudle:NSObject{
-    func handleResponse(response:String?,success:((_ response:String?)->Void)?,failed:((_ code: Int,_ message:String)->())?) {
-        let responseJson = response?.toJson()
-        let code = responseJson?["code"] as? Int ?? -1
-        let _ = responseJson?["error"] as? String ?? ""
-        let message = responseJson?["message"] as? String ?? ""
-        let status = responseJson?["status"] as? String ?? ""
-        let data = responseJson?["data"] as? [String:Any]
-        let dataString = data?.toString()
-        DispatchQueue.main.async {
-            if code == 0 {
-                success?(dataString)
-            }else{
-                failed?(code,message)
-            }
-        }
-    }
-}
-
-
 @objc public class MirrorMarketplaceMoudle: MirrorBaseMoudle {
     
     var config:MirrorWorldSDKConfig?
