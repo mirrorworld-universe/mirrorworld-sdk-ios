cdcdcd3b940c4b3fff56167322fded80db2442b3
Squall <2573040560@qq.com>
2023-07-10 13:30:17 +0800

    update version

--- MirrorWorldSDK/Classes/MirrorWorldSDK.swift
+++ MirrorWorldSDK/Classes/MirrorWorldSDK.swift
@@ -13,7 +13,7 @@ public let MWSDK = MirrorWorldSDK.share
 @objc public class MirrorWorldSDK: NSObject {
    @objc public static let share = MirrorWorldSDK()
     
-    public var SDKVersion:String = "2.1.4"
+    public var SDKVersion:String = "2.2.0"
     
     public typealias loginListent = ((_ s:Bool)->Void)?
 
